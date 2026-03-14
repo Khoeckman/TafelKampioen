26d5406d70b8fa5f6b57b99c85a7624781fe65ac
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2026-03-14 02:17:04 +0100

    Test build with assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   basePath: '/TafelKampioen',
+  assetPrefix: '/TafelKampioen/',
   distDir: 'docs',
   output: 'export',
 }
